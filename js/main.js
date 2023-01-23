b287f6cde83744fa7a920169ed49ee4ca4e5222d
ADB <91122732+pgm-arnadebu@users.noreply.github.com>
2023-01-23 11:57:20 +0100

    24/01/2023: Create user flow

--- js/main.js
+++ js/main.js
@@ -20,7 +20,14 @@ const app = {
     this.$launchDancingLector = document.querySelector('.launch-lector-app');
     this.$dancingLectorUI = document.querySelector('.dancing-lector-app');
     this.$reticle = document.querySelector("[ar-hit-test]");
-    this.$animationBtn = document.querySelector('.animate');
+    this.$sambaBtn = document.querySelector('.samba');
+    this.$hipHopBtn = document.querySelector('.hiphop');
+    this.$bellyBtn = document.querySelector('.belly');
+    this.$swingBtn = document.querySelector('.swing');
+    this.$flairBtn = document.querySelector('.flair');
+    this.$sillyBtn = document.querySelector('.silly');
+    this.$twerkBtn = document.querySelector('.twerk');
+    this.$idleBtn = document.querySelector('.idle');
     this.addEventListeners();
   },
   addEventListeners() {
@@ -41,6 +48,7 @@ const app = {
 
     // When entering AR mode...
     scene.addEventListener("enter-vr", () => {
+
       // Show splash screen
       this.$splash.classList.remove('is-hidden');
 
@@ -63,6 +71,7 @@ const app = {
       reticle.setAttribute('visible', 'false');
     });
 
+
     reticle.addEventListener('select', function () {
       if (this.components["ar-hit-test"].hasFoundAPose) {
         positionObject();
@@ -73,15 +82,35 @@ const app = {
     // Remove splash screen and add UI for virtual pet application
     document.querySelector('.splash').classList.add('is-hidden');
     document.querySelector('.virtual-pet-app').classList.remove('is-hidden');
+    document.querySelector('#helena').classList.add('is-hidden');
 
     // Get model and show object when screen is selected
-    const object = document.getElementById('eagle');
+    const object = document.getElementById('goose');
     app.generatePositionObject(object);
 
     // Add animation when button is clicked
-    app.$animationBtn.addEventListener('click', () => {
-      object.setAttribute('animation-mixer', 'clip:fly; loop:infinite; timeScale: .3; crossFadeDuration: 2;')
-    })
+    app.$sambaBtn.addEventListener('click', () => app.addAnimation(object, 'samba'));
+    app.$hipHopBtn.addEventListener('click', () => app.addAnimation(object, 'hiphop'));
+    app.$bellyBtn.addEventListener('click', () => app.addAnimation(object, 'belly'));
+    app.$swingBtn.addEventListener('click', () => app.addAnimation(object, 'swing'));
+  },
+  generateDancingLectorApp() {
+    // Remove splash screen and add UI for virtual pet application
+    document.querySelector('.splash').classList.add('is-hidden');
+    document.querySelector('.dancing-lector-app').classList.remove('is-hidden');
+
+    // Get model and show object when screen is selected
+    const object = document.getElementById('helena');
+    app.generatePositionObject(object);
+
+    // Add animation when button is clicked
+    app.$flairBtn.addEventListener('click', () => app.addAnimation(object, 'flair'));
+    app.$sillyBtn.addEventListener('click', () => app.addAnimation(object, 'sillydance1'));
+    app.$twerkBtn.addEventListener('click', () => app.addAnimation(object, 'twerk'));
+    app.$idleBtn.addEventListener('click', () => app.addAnimation(object, 'idle'));
+  },
+  addAnimation(object, animation) {
+    object.setAttribute('animation-mixer', `clip:${animation}; loop:infinite; crossFadeDuration: 2;`);
   },
   generatePositionObject(object) {
     const $reticle = this.$reticle;
@@ -93,7 +122,6 @@ const app = {
       }
     });
   },
-  generateDancingLectorApp() { },
 
 }
 
